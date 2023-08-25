2429128c4d755b0724611a698561bc1ad0dd8c52
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-08-25 12:04:26 +0200

    Interfaccia Observer e Subject

---
+++ src/main/java/Epicode/U5S3ProvaSettimanale/Interface/Observer.java
@@ -0,0 +1,7 @@
+package Epicode.U5S3ProvaSettimanale.Interface;
+
+public interface Observer {
+	
+	public void controlloFumo();
+
+}
---
+++ src/main/java/Epicode/U5S3ProvaSettimanale/Interface/Subject.java
@@ -0,0 +1,9 @@
+package Epicode.U5S3ProvaSettimanale.Interface;
+
+public interface Subject {
+	
+	public void registraSonda(Observer o);
+	
+	public void notifica(Observer o);
+
+}
