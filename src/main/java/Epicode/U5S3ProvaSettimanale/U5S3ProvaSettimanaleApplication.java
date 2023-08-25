3ae41351e4fc4a652a844bb09c81cf0013d73dd7
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-08-25 10:59:49 +0200

    repo opening

---
+++ src/main/java/Epicode/U5S3ProvaSettimanale/U5S3ProvaSettimanaleApplication.java
@@ -0,0 +1,13 @@
+package Epicode.U5S3ProvaSettimanale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S3ProvaSettimanaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S3ProvaSettimanaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Epicode/U5S3ProvaSettimanale/U5S3ProvaSettimanaleApplicationTests.java
@@ -0,0 +1,13 @@
+package Epicode.U5S3ProvaSettimanale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S3ProvaSettimanaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
